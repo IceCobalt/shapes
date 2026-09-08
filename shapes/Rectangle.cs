2a045b997ead8ecb87d7fb210f45952fbdbf00e1
Cobalt <ethan.hahn@trojans.dsu.edu>
2026-09-08 15:26:38 -0500

    added rectangle class

--- shapes/Rectangle.cs
+++ shapes/Rectangle.cs
@@ -4,5 +4,23 @@ using System.Text;
 
 namespace shapes {
     public class Rectangle {
+        //Attributes
+        public double Height;
+        public double Width;
+
+        //Constructor
+        public Rectangle() {
+            Console.WriteLine("A new rectangle touches the beacon");
+            Height = 0;
+            Width = 0;
+        }
+
+        //Methods | Functions
+        public double CalculateArea() {
+            return Width * Height;
+        }
+        public double CalculatePerimeter() {
+            return (Width * 2) + (Height * 2);
+        }
     }
 }

e190310b6fea440cb7104c38d524910313a6d836
Cobalt <ethan.hahn@trojans.dsu.edu>
2026-09-08 15:17:41 -0500

    Add project files.

---
+++ shapes/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shapes {
+    internal class Class1 {
+    }
+}
---
+++ shapes/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace shapes {
+    internal class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine("Hello, 0World!");
+        }
+    }
+}

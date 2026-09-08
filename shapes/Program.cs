1985b20be2c8ef3e5f4b7d5046b8118c30964e6b
Cobalt <ethan.hahn@trojans.dsu.edu>
2026-09-08 15:36:03 -0500

    added parameterized and coyp constructors

--- shapes/Program.cs
+++ shapes/Program.cs
@@ -2,6 +2,17 @@
     internal class Program {
         static void Main( string[] args ) {
             Console.WriteLine("Hello, World!");
+
+            Rectangle r1 = new Rectangle();
+            r1.Height = 14.5;
+            r1.Width = 10;
+
+            Console.WriteLine($"Height is {r1.Height} and width is {r1.Width}");
+            Console.WriteLine($"Perimeter is {r1.CalculatePerimeter()}");
+            Console.WriteLine($"Area is {r1.CalculateArea()}");
+
+            Rectangle r2 = new Rectangle(15,15);
+            Rectangle r3 = new Rectangle(r2);
         }
     }
 }
--- shapes/Rectangle.cs
+++ shapes/Rectangle.cs
@@ -9,12 +9,24 @@ namespace shapes {
         public double Width;
 
         //Constructor
+        //Default Constructor
         public Rectangle() {
             Console.WriteLine("A new rectangle touches the beacon");
             Height = 0;
             Width = 0;
         }
-
+        //Parameterized Constructor
+        public Rectangle(double width, double height) {
+            Console.WriteLine("A new rectangle touches the beacon with intent");
+            Height = height;
+            Width = width;
+        }
+        //Copy Constructor
+        public Rectangle(Rectangle r) {
+            Console.WriteLine("A familiar rectangle touches the beacon with intent");
+            Height = r.Height;
+            Width = r.Width;
+        }
         //Methods | Functions
         public double CalculateArea() {
             return Width * Height;

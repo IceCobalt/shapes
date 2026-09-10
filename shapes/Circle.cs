4aa7dac3420da312e2b887de9d8d7d66fecd0872
Cobalt <ethan.hahn@trojans.dsu.edu>
2026-09-10 14:50:42 -0500

    added support for circles

---
+++ shapes/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shapes {
+    internal class Circle {
+        //Attributes
+        public double Radius;
+
+        //Constructor
+        public Circle() {
+            Radius = 0;
+            Console.WriteLine($"A new circle, yet to form, touches the beacon");
+        }
+
+        public Circle(double Radius) {
+            Console.WriteLine($"A new circle at about {Radius}cm touches the beacon");
+        }
+        public Circle(Circle original) {
+            Radius = original.Radius;
+            Console.WriteLine($"A familiar circle of {Radius}cm touches the beacon");
+        }
+
+        //Methods
+        public double CalculateArea(double Radius) {
+            return Math.PI * Math.Pow(Radius,2); 
+        }
+
+        public double CalculatePerimeter(double Radius) {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
